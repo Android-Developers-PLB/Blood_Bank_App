432fd01e70ed92029147caffe2c8014622198880
Stellar-X <shubhamchaudhary632001@gmail.com>
2021-02-22 22:23:00 +0530

    Update-1

--- app/src/main/java/com/example/blood_bank_app/MainActivity.java
+++ app/src/main/java/com/example/blood_bank_app/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.blood_bank_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Called when user taps on 'Register' TextView
+    public void toRegister(View view) {
+        Toast.makeText(MainActivity.this,"Button Clicked!!", Toast.LENGTH_SHORT).show();
+        Intent myIntent = new Intent(this, RegisterActivity.class);
+        startActivity(myIntent);
     }
 }
