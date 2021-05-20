708d43c303a80570fcd4238cf9662a936454b073
Stellar-X <shubhamchaudhary632001@gmail.com>
2021-05-20 14:32:54 +0530

    Added Create Request Page Frontend

--- app/src/main/java/com/example/blood_bank_app/LifeSavers/Activities/AboutUs.java
+++ app/src/main/java/com/example/blood_bank_app/LifeSavers/Activities/AboutUs.java
@@ -1,5 +1,10 @@
 package com.example.blood_bank_app.LifeSavers.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +13,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.blood_bank_app.R;
 import com.google.android.material.navigation.NavigationView;
-
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.MenuItem;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -79,7 +79,7 @@ public class AboutUs extends AppCompatActivity {
     }
     public void setUpToolbar() {
         drawerLayout = findViewById(R.id.drawerLayout);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
---
+++ app/src/main/java/com/example/blood_bank_app/LifeSavers/Activities/CreateRequestActivity.java
@@ -0,0 +1,16 @@
+package com.example.blood_bank_app.LifeSavers.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.blood_bank_app.R;
+
+public class CreateRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_request);
+    }
+}
